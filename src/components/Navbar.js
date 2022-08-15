ad62cabcf1c9640ad41b4907482d03991f7d323e
mihir hadavani <miheerhadvani990@gmail.com>
2022-08-15 18:03:22 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,41 @@
 // import PropTypes from 'prop-types'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 const Navbar = (p)=>{
 
     return (   
       <>
       <nav className="navbar fixed-top navbar-expand-lg bg-dark navbar-dark">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">NewsMonkey</Link>
+    <NavLink exact  className="navbar-brand" to="/">NewsMonkey</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+          <NavLink exact className="nav-Navlink active" aria-current="page" to="/">Home</NavLink>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/about">About</Link>
+          <NavLink exact className="nav-Navlink" to="/about">About</NavLink>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/business">business</Link>
+          <NavLink exact className="nav-Navlink" to="/business">business</NavLink>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/">general</Link>
+          <NavLink exact className="nav-Navlink" to="/">general</NavLink>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/health">health</Link>
+          <NavLink exact className="nav-Navlink" to="/health">health</NavLink>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/science">science</Link>
+          <NavLink exact className="nav-Navlink" to="/science">science</NavLink>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/sports">sports</Link>
+          <NavLink exact className="nav-Navlink" to="/sports">sports</NavLink>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/technology">technology</Link>
+          <NavLink exact className="nav-Navlink" to="/technology">technology</NavLink>
         </li>
       </ul>      
     </div>
